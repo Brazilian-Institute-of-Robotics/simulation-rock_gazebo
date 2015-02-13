15ebdeb92edb29a61b62e3e9097b40d58c1a63ae
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-02-13 14:58:28 -0200

    reindent to 4 spaces, no tabs
    This is something that I usually avoid, but it is more than required
    for this package.

--- src/RockBridge.cpp
+++ src/RockBridge.cpp
@@ -33,36 +33,36 @@ using namespace rock_gazebo;
 
 void RockBridge::Load(int _argc , char** _argv)
 {
-	RTT::corba::ApplicationServer::InitOrb(_argc, _argv);
+    RTT::corba::ApplicationServer::InitOrb(_argc, _argv);
     RTT::corba::TaskContextServer::ThreadOrb(ORO_SCHED_OTHER, RTT::os::LowestPriority, 0);
-	
-	// Import typekits to allow RTT convert the types used by the components
-	RTT::types::TypekitRepository::Import(new orogen_typekits::stdTypekitPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::stdCorbaTransportPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::stdMQueueTransportPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::stdTypelibTransportPlugin);
-	
-	RTT::types::TypekitRepository::Import(new orogen_typekits::baseTypekitPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::baseCorbaTransportPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::baseMQueueTransportPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::baseTypelibTransportPlugin);
-	
-	RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboTypekitPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboCorbaTransportPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboMQueueTransportPlugin);
-	RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboTypelibTransportPlugin);
-	
-		
-	// Each simulation step the Update method is called to update the simulated sensors and actuators
-	eventHandler.push_back(
-			event::Events::ConnectWorldUpdateBegin(
-					boost::bind(&RockBridge::updateBegin,this, _1)));
-	eventHandler.push_back(
-			event::Events::ConnectWorldUpdateEnd(
-					boost::bind(&RockBridge::updateEnd,this)));
-	eventHandler.push_back(
-			event::Events::ConnectWorldCreated(
-					boost::bind(&RockBridge::worldCreated,this, _1)));
+
+    // Import typekits to allow RTT convert the types used by the components
+    RTT::types::TypekitRepository::Import(new orogen_typekits::stdTypekitPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::stdCorbaTransportPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::stdMQueueTransportPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::stdTypelibTransportPlugin);
+
+    RTT::types::TypekitRepository::Import(new orogen_typekits::baseTypekitPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::baseCorbaTransportPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::baseMQueueTransportPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::baseTypelibTransportPlugin);
+
+    RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboTypekitPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboCorbaTransportPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboMQueueTransportPlugin);
+    RTT::types::TypekitRepository::Import(new orogen_typekits::gazeboTypelibTransportPlugin);
+
+
+    // Each simulation step the Update method is called to update the simulated sensors and actuators
+    eventHandler.push_back(
+            event::Events::ConnectWorldUpdateBegin(
+                boost::bind(&RockBridge::updateBegin,this, _1)));
+    eventHandler.push_back(
+            event::Events::ConnectWorldUpdateEnd(
+                boost::bind(&RockBridge::updateEnd,this)));
+    eventHandler.push_back(
+            event::Events::ConnectWorldCreated(
+                boost::bind(&RockBridge::worldCreated,this, _1)));
 }
 //======================================================================================
 // worldCreated() is called every time a world is added
@@ -81,21 +81,21 @@ void RockBridge::worldCreated(std::string const& worldName)
     world_task->setGazeboWorld(world);
     setupTaskActivity(world_task);
     tasks.push_back(world_task);
-		    
+
     typedef physics::Model_V Model_V;
     Model_V model_list = world->GetModels();
-	for(Model_V::iterator model_it = model_list.begin(); model_it != model_list.end(); ++model_it)
-	{
-		createTask(world,*model_it);
-	}
-	model_list.clear();
+    for(Model_V::iterator model_it = model_list.begin(); model_it != model_list.end(); ++model_it)
+    {
+        createTask(world,*model_it);
+    }
+    model_list.clear();
 }
 
 void RockBridge::setupTaskActivity(RTT::TaskContext* task)
 {
     // Export the component interface on CORBA to Ruby access the component
     RTT::corba::TaskContextServer::Create( task );
-	
+
     // Set up the component activity_signal
     RTT::extras::SequentialActivity* activity =
         new RTT::extras::SequentialActivity(task->engine());
@@ -107,10 +107,10 @@ void RockBridge::setupTaskActivity(RTT::TaskContext* task)
 //======================================================================================
 void RockBridge::createTask(physics::WorldPtr world, physics::ModelPtr model)
 {
-	std::cout << " ~ " << std::endl;
-	gzmsg << "RockBridge: initializing model: "<< (model)->GetName() << std::endl;
+    std::cout << " ~ " << std::endl;
+    gzmsg << "RockBridge: initializing model: "<< (model)->GetName() << std::endl;
 
-        // Create and initialize one rock component for each gazebo model
+    // Create and initialize one rock component for each gazebo model
     ModelTask* task = new ModelTask();
     task->setGazeboModel(world, model);
     setupTaskActivity(task);
@@ -137,21 +137,21 @@ RockBridge::RockBridge()
 //======================================================================================
 RockBridge::~RockBridge()
 {
-	// Delete pointers to activity
-	for(Activities::iterator activity_it = activities.begin(); 
-		    activity_it != activities.end(); ++activity_it)
-	{
-		delete *activity_it;
-	}
-	activities.clear();
-	
-	// Delete pointers to tasks
-	for(Tasks::iterator task_it = tasks.begin();
-		    task_it != tasks.end(); ++task_it)
-	{
-		delete *task_it;
-	}
-	tasks.clear();
+    // Delete pointers to activity
+    for(Activities::iterator activity_it = activities.begin(); 
+            activity_it != activities.end(); ++activity_it)
+    {
+        delete *activity_it;
+    }
+    activities.clear();
+
+    // Delete pointers to tasks
+    for(Tasks::iterator task_it = tasks.begin();
+            task_it != tasks.end(); ++task_it)
+    {
+        delete *task_it;
+    }
+    tasks.clear();
 
     RTT::corba::TaskContextServer::ShutdownOrb();
     RTT::corba::TaskContextServer::DestroyOrb();	
--- src/RockBridge.hpp
+++ src/RockBridge.hpp
@@ -30,33 +30,33 @@ namespace gazebo
 namespace rock_gazebo
 {
 
-	class RockBridge: public gazebo::SystemPlugin
-	{
-		public:
-			// Pure virtual function implementation
-			virtual void Load(int _argc = 0, char** _argv = NULL);
-//			virtual void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
-			RockBridge(); 
-			~RockBridge();
-			
-		private:
-			void worldCreated(std::string const&);
-//			void modelAdded(std::string const&);
-			void createTask(gazebo::physics::WorldPtr, gazebo::physics::ModelPtr); 
-			void updateBegin(gazebo::common::UpdateInfo const& info);
-			void updateEnd();
+    class RockBridge: public gazebo::SystemPlugin
+    {
+        public:
+            // Pure virtual function implementation
+            virtual void Load(int _argc = 0, char** _argv = NULL);
+            //			virtual void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
+            RockBridge(); 
+            ~RockBridge();
+
+        private:
+            void worldCreated(std::string const&);
+            //			void modelAdded(std::string const&);
+            void createTask(gazebo::physics::WorldPtr, gazebo::physics::ModelPtr); 
+            void updateBegin(gazebo::common::UpdateInfo const& info);
+            void updateEnd();
             void setupTaskActivity(RTT::TaskContext* task);
 
-			std::vector<gazebo::event::ConnectionPtr> eventHandler;
+            std::vector<gazebo::event::ConnectionPtr> eventHandler;
 
-          typedef std::vector<RTT::TaskContext*> Tasks;
-          Tasks tasks;
-			typedef std::vector<RTT::base::ActivityInterface*> Activities;
-			Activities activities;
-	};
-	
- 	// Register this plugin with the simulator
-	GZ_REGISTER_SYSTEM_PLUGIN(RockBridge)
+            typedef std::vector<RTT::TaskContext*> Tasks;
+            Tasks tasks;
+            typedef std::vector<RTT::base::ActivityInterface*> Activities;
+            Activities activities;
+    };
+
+    // Register this plugin with the simulator
+    GZ_REGISTER_SYSTEM_PLUGIN(RockBridge)
 
 } // end namespace rock_gazebo
 
