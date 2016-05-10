34e94d81c278d522d5550638899cc5b8514ff806
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-05-10 20:28:32 -0300

    syskit: fix referral to frames that don't exist
    Only the robot model is loaded in the transformer, meaning that the
    system does not know about the other models in the scene. However,
    the code was referring to these models are valid frame names, which
    - now that the transformer does proper validation - fails.

--- bindings/ruby/lib/rock_gazebo/syskit/robot_definition_extension.rb
+++ bindings/ruby/lib/rock_gazebo/syskit/robot_definition_extension.rb
@@ -54,11 +54,11 @@ module RockGazebo
             # @raise [ArgumentError] if models does not contain robot_model
             def load_sdf(robot_model, world_name: 'world', name: robot_model.name, models: [robot_model])
                 @sdf_world_name = world_name.to_str
-
-                expose_sdf_models(models, world_name: world_name)
                 if !models.any? { |m| m.name == name }
                     raise ArgumentError, "the set of models given to #load_sdf has no model named #{name}"
                 end
+
+                expose_sdf_models(models, world_name: world_name)
                 load_sdf_robot_model(robot_model, name: name, world_name: world_name)
             end
 
@@ -70,8 +70,7 @@ module RockGazebo
                     device(Rock::Devices::Gazebo::Model, as: m.name,
                            using: OroGen::RockGazebo::ModelTask).
                            prefer_deployed_tasks(/^gazebo:#{world_name}:#{m.name}$/).
-                           advanced.
-                           frame_transform(m.name => world_name)
+                           advanced
                 end
             end
 
