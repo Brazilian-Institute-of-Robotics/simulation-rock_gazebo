4eee8c8c4bfd708e9ccf55317c8ce385847ab834
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-29 11:46:10 -0200

    syskit: make the world be 'world'
    There can be only one world in the syskit context, and there's no
    point in making our life miserable like this.

--- bindings/ruby/lib/rock_gazebo/syskit/robot_definition_extension.rb
+++ bindings/ruby/lib/rock_gazebo/syskit/robot_definition_extension.rb
@@ -2,13 +2,6 @@ module RockGazebo
     module Syskit
         # Gazebo-specific extensions to {Syskit::Robot::RobotDefinition}
         module RobotDefinitionExtension
-            # The name of the SDF world loaded with {#load_sdf}
-            #
-            # The default is 'world'
-            #
-            # @return [String]
-            attr_reader :sdf_world_name
-
             # Given a sensor, returns the device and device driver model that
             # should be used to handle it
             #
@@ -17,7 +10,7 @@ module RockGazebo
             #   rock-gazebo plugin or by the syskit integration (yet), or the
             #   device model and device driver that should be used for this
             #   sensor
-            def sensors_to_device(sensor, device_name, frame_name, world_name: 'world')
+            def sensors_to_device(sensor, device_name, frame_name)
                 case sensor.type
                 when 'ray'
                     require 'rock/models/devices/gazebo/ray'
@@ -26,7 +19,7 @@ module RockGazebo
                 when 'imu'
                     require 'rock/models/devices/gazebo/imu'
                     device(Rock::Devices::Gazebo::Imu, as: device_name, using: OroGen::RockGazebo::ImuTask).
-                        frame_transform(frame_name => world_name)
+                        frame_transform(frame_name => 'world')
                 when 'camera'
                     require 'rock/models/devices/gazebo/camera'
                     device(Rock::Devices::Gazebo::Camera, as: device_name, using: OroGen::RockGazebo::CameraTask).
@@ -44,8 +37,6 @@ module RockGazebo
             # sensors only for 'model'
             #
             # @param [SDF::Model] robot_model the SDF model for this robot
-            # @param [String] world_name the name of the SDF world we are
-            #   running in
             # @param [String] name the name of the model in the world, if it
             #   differs from the robot_model name (e.g. if you have multiple
             #   instances of the same robot)
@@ -55,24 +46,23 @@ module RockGazebo
             #   robot_model
             # @return [void]
             # @raise [ArgumentError] if models does not contain robot_model
-            def load_gazebo(robot_model, world_name: 'world', name: robot_model.name, models: [robot_model])
-                @sdf_world_name = world_name.to_str
+            def load_gazebo(robot_model, deployment_prefix, name: robot_model.name, models: [robot_model])
                 if !models.any? { |m| m.name == name }
                     raise ArgumentError, "the set of models given to #load_gazebo has no model named #{name}"
                 end
 
-                expose_gazebo_models(models, world_name: world_name)
-                load_gazebo_robot_model(robot_model, name: name, world_name: world_name)
+                expose_gazebo_models(models, deployment_prefix)
+                load_gazebo_robot_model(robot_model, deployment_prefix, name: name)
             end
 
             # @api private
             #
             # Define devices for each model in the world
-            def expose_gazebo_models(models, world_name: 'world')
+            def expose_gazebo_models(models, deployment_prefix)
                 models.each do |m|
                     device(Rock::Devices::Gazebo::Model, as: m.name,
                            using: OroGen::RockGazebo::ModelTask).
-                           prefer_deployed_tasks(/^gazebo:#{world_name}:#{m.name}$/).
+                           prefer_deployed_tasks("#{deployment_prefix}:#{m.name}").
                            advanced
                 end
             end
@@ -80,7 +70,7 @@ module RockGazebo
             # @api private
             #
             # Define devices for all links and sensors in the model
-            def load_gazebo_robot_model(model, name: model.name, world_name: 'world')
+            def load_gazebo_robot_model(model, deployment_prefix, name: model.name)
                 driver_m = OroGen::RockGazebo::ModelTask
                 find_device(model.name).advanced = false
                 model.each_link do |l|
@@ -89,17 +79,17 @@ module RockGazebo
                     driver_srv = link_driver_m.require_dynamic_service(
                         'link_export', as: "#{l.name}_link", frame_basename: frame_basename)
                     link_driver_m = link_driver_m.to_instance_requirements.
-                        prefer_deployed_tasks(/^gazebo:#{world_name}:#{name}$/).
+                        prefer_deployed_tasks("#{deployment_prefix}:#{name}").
                         with_arguments('model_dev' => find_device(name)).
                         use_frames("#{frame_basename}_source" => l.full_name,
-                                   "#{frame_basename}_target" => world_name).
+                                   "#{frame_basename}_target" => 'world').
                         select_service(driver_srv)
                     device(Rock::Devices::Gazebo::Link, as: "#{l.name}_link", using: link_driver_m).
                         advanced
                 end
                 model.each_sensor do |s|
-                    if device = sensors_to_device(s, "#{s.name}_sensor", s.parent.full_name, world_name: world_name)
-                        device.prefer_deployed_tasks(/^gazebo:#{world_name}:#{name}:#{s.name}$/)
+                    if device = sensors_to_device(s, "#{s.name}_sensor", s.parent.full_name)
+                        device.prefer_deployed_tasks("#{deployment_prefix}:#{name}:#{s.name}")
                     else
                         RockGazebo.warn "Robot#load_gazebo: don't know how to handle sensor #{s.full_name} of type #{s.type}"
                     end
